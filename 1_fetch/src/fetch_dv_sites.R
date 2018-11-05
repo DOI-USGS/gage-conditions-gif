917cdf356c2a9238aa89a4bb078afece40c24eb3
Carr <lcarr@usgs.gov>
2018-11-05 08:34:20 -0600

    fetch sites

---
+++ 1_fetch/src/fetch_dv_sites.R
@@ -0,0 +1,27 @@
+#' @title Fetch appropriate daily value sites from NWIS
+#' 
+#' @param ind_file character file name where the output should be saved
+#' @param dates object from viz_config.yaml that specifies dates as string
+fetch.dv_sites <- function(ind_file, dates){
+
+  hucs <- zeroPad(1:21, 2) # all hucs
+  
+  sites <- c()
+  for(huc in hucs){
+    sites <- 
+      dataRetrieval::whatNWISdata(
+        huc = huc, 
+        service = "dv", 
+        startDate = dates$start,
+        endDate = dates$end,
+        parameterCd = "00060", 
+        statCd = "00003") %>% 
+      dplyr::pull(site_no) %>% 
+      c(sites)
+  }
+  
+  # Write the data file and the indicator file
+  data_file <- as_data_file(ind_file)
+  saveRDS(sites, data_file)
+  gd_put(ind_file, data_file)
+}
