af72cff7563182f8cdb890c51fa0d63e17ff8226
wdwatkins <wwatkins@usgs.gov>
2018-11-06 16:03:51 -0600

    missed some comments

--- 2_process/src/process_site_locations_to_sp.R
+++ 2_process/src/process_site_locations_to_sp.R
@@ -1,7 +1,7 @@
 #' @title Generate a spatial points data.frame for site locations
 #'
-#' @param ind_file character file name where the output should be saved
 #' @param site_locations_ind indicator file for the vector of site numbers
+#' @param projection projection string
 process_site_locations_to_sp <- function(site_locations_ind, projection){
 
   site_data <- readRDS(scipiper::sc_retrieve(site_locations_ind, remake_file = '1_fetch.yml'))
@@ -10,10 +10,6 @@ process_site_locations_to_sp <- function(site_locations_ind, projection){
     sp::spTransform(CRS(projection)) %>%
     sp::SpatialPointsDataFrame(data = site_data[c('site_no', 'STATEFP')])
   return(sp_sites)
-  # Write the data file and the indicator file
-  # data_file <- scipiper::as_data_file(ind_file)
-  # saveRDS(sp_sites, data_file)
-  # scipiper::gd_put(ind_file, data_file)
 }
 
 
