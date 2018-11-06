9fbae1b5e5af595ef30038d14bf47ae7940a6b1b
Alison Appling <aappling@usgs.gov>
2018-11-06 18:10:26 -0500

    flesh out basemap_fun

--- 6_visualize/src/prep_basemap_fun.R
+++ 6_visualize/src/prep_basemap_fun.R
@@ -1,11 +1,9 @@
-prep_basemap_fun <- function(state_boundaries_ind, basemap_cfg) {
-
-  state_boundaries_sp <- readRDS(sc_retrieve(state_boundaries_ind, '2_process.yml'))
+prep_basemap_fun <- function(states_shifted, basemap_cfg) {
 
   plot_fun <- function(){
 
-    plot(state_boundaries_sp, add=TRUE,
-         lwd=basemap_cfg$lwd, col=basemap_cfg$bg, border=basemap_cfg$border)
+    plot(states_shifted, add=TRUE,
+         lwd=basemap_cfg$lwd, col=basemap_cfg$col, border=basemap_cfg$border)
 
   }
   return(plot_fun)
