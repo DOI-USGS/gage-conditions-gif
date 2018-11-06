61aec5c7b24a8e1082de483ce77c4dfe51f7dd26
Alison Appling <aappling@usgs.gov>
2018-11-06 17:03:57 -0500

    start on prep_basemap_fun

--- 6_visualize/src/prep_basemap_fun.R
+++ 6_visualize/src/prep_basemap_fun.R
@@ -1,18 +1,12 @@
-prep_basemap_fun <- function(focus_geoms_ind, secondary_geoms_ind = NULL, detail_geoms_ind = NULL){
+prep_basemap_fun <- function(state_boundaries_ind, basemap_cfg) {
+
+  state_boundaries_sp <- readRDS(sc_retrieve(state_boundaries_ind, '2_process.yml'))
 
   plot_fun <- function(){
-    if (!is.null(secondary_geoms_ind)){
-      secondary_geoms <- readRDS(sc_retrieve(secondary_geoms_ind))
-      plot(secondary_geoms, add = TRUE, lwd = 0.3, col = 'grey80', border = 'grey75') # should style args be in a config?
-    }
 
-    if (!is.null(detail_geoms_ind)){
-      detail_geoms <- readRDS(sc_retrieve(detail_geoms_ind))
-      plot(detail_geoms, add = TRUE, lwd = 0.3, col = NA, border = 'grey95') # should style args be in a config?
-    }
+    plot(state_boundaries_sp, add=TRUE,
+         lwd=basemap_cfg$lwd, col=basemap_cfg$bg, border=basemap_cfg$border)
 
-    focus_geoms <- readRDS(sc_retrieve(focus_geoms_ind))
-    plot(focus_geoms, add = TRUE, col = "grey95", border = 'grey75')
   }
   return(plot_fun)
 }
