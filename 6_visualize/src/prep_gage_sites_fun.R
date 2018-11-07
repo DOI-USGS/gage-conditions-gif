2a339075c46c89e870e58ee5e720bc43b1858fd7
wdwatkins <wwatkins@usgs.gov>
2018-11-07 15:15:24 -0600

    gage sites used in complete pngs

--- 6_visualize/src/prep_gage_sites_fun.R
+++ 6_visualize/src/prep_gage_sites_fun.R
@@ -1,11 +1,13 @@
-prep_gage_sites_fun <- function(percentile_color_data, sites_sp, gage_style, date){
-  this_date <- as.Date(date) #watch timezones if we ever switch from daily data
+prep_gage_sites_fun <- function(percentile_color_data_ind, sites_sp, gage_style, dateTime){
+  this_date <- as.POSIXct(dateTime, tz = "UTC") #watch timezones if we ever switch from daily data
   #only plotting sites with color for now
+  percentile_color_data <- readRDS(as_data_file(percentile_color_data_ind))
   this_date_colors <- filter(percentile_color_data, dateTime == this_date)
   sites_sp@data <- left_join(sites_sp@data, this_date_colors, by = "site_no")
-  plot_fun <- function(){
+  gage_style <- gage_style$gage_style
+  gage_sites_plot_fun <- function(){
     #single plot of gages w/color
     plot(sites_sp, add = TRUE, pch = gage_style$pch, col = sites_sp$color, cex = gage_style$cex)
   }
-  return(plot_fun)
+  return(gage_sites_plot_fun)
 }
