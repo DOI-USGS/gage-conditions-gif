cee0bb053f5b1fe430c2f684ead0fc961da31fcb
Carr <lcarr@usgs.gov>
2018-11-07 15:29:33 -0600

    legend!

--- 6_visualize/src/prep_legend_fun.R
+++ 6_visualize/src/prep_legend_fun.R
@@ -1,94 +1,74 @@
 
-prep_legend_fun <- function(precip_bins, legend_styles, timesteps_ind, storm_points_sf, DateTime=NA, x_pos = c('left', 'right'), y_pos = c('bottom','top'), legend_text_cfg){
+prep_legend_fun <- function(percentiles_str, sites_color_palette,
+                            x_pos = c('left', 'right'), y_pos = c('bottom','top'),
+                            legend_cfg){
 
   x_pos <- match.arg(x_pos)
   y_pos <- match.arg(y_pos)
 
-  if(is.na(DateTime)) {
-    timesteps <- readRDS(sc_retrieve(timesteps_ind))
-    DateTime <- timesteps[1]
-    rm(timesteps)
-  }
-  this_DateTime <- as.POSIXct(DateTime, tz = "UTC") # WARNING, IF WE EVER MOVE FROM UTC elsewhere, this will be fragile/bad.
+  col_fun <- colorRamp(sites_color_palette)
+  percentiles <- as.numeric(percentiles_str)/100
+  legend_cols <- sapply(percentiles, FUN = function(x){
+    rgb(col_fun(x), maxColorValue = 255)
+  })
 
-  hurricane_col <- NA
-  if(!is.null(storm_points_sf)) {
-    this_dot <- filter(storm_points_sf, DateTime == this_DateTime)
-    if(nrow(this_dot) > 0) {
-      hurricane_col <- legend_styles$hurricane_cols[(this_dot$SS + 1)]
-      hurricane_cat <- legend_styles$hurricane_col_names[(this_dot$SS + 1)]
-    }
-  }
-
-  rm(storm_points_sf)
+  alpha_hex <- 'CC'
 
   plot_fun <- function(){
 
     # compute position info shared across multiple legend elements
     coord_space <- par()$usr
-    bin_w_perc <- 0.05 # percentage of X domain
-    bin_h_perc <- 0.02 # *also* percentage of X domain
-    bin_w <- bin_w_perc * diff(coord_space[c(1,2)])
-    bin_h <- bin_h_perc * diff(coord_space[c(1,2)])
+
+    # percentage of X domain for circle diameter; assumes 175 is max cex
+    # used for spacing circles along the x and y directions
+    point_perc <- legend_cfg$point_cex/175
+    point_dia_usr <- point_perc*diff(coord_space[1:2])
+
+    # Legend text
+    left_text <- "Lower flows"
+    mid_text <- "Normal"
+    right_text <- "Higher flows"
+
     if (x_pos == 'left'){
-      txt_pos = 4
-      x_edge <- coord_space[1]
-      shift_dir <- 1
+      x_edge <- coord_space[1] + strwidth(left_text)
+      shift_xdir <- 1
     } else if (x_pos == 'right'){
-      txt_pos = 2
-      x_edge <- coord_space[2]
-      shift_dir <- -1
-    }
-    ybottom <- coord_space[3]
-    dot_x <- x_edge+bin_w/2*shift_dir
-    dot_txt_x <- x_edge+bin_w*0.7*shift_dir
-    seg_x <- x_edge+bin_w/3*shift_dir
-    center_to_txt_y <- strheight("A")/3 # height of character divided by three seems to do the trick
-
-    # plot precip bins and precip label
-    precip_txt_y <- ybottom+2*bin_h*0.8
-    text(x_edge, precip_txt_y, labels = 'NOAA total rainfall amount (inches)', pos = txt_pos,
-         cex=legend_text_cfg$cex, col=legend_text_cfg$col, family=legend_text_cfg$family)
-    if (x_pos == 'left'){
-      bin_j <- 1:nrow(precip_bins)
-      xright <- x_edge+bin_w
-    } else if (x_pos == 'right'){
-      bin_j <- nrow(precip_bins):1
-      xright <- x_edge
-    }
-    for (j in bin_j){
-      col <- as.character(precip_bins$col[j])
-      text_col <- ifelse(any(col2rgb(col) < 130), 'white','black')
-      rect(xleft = xright-bin_w, ybottom = ybottom, xright = xright, ytop = ybottom+bin_h, col = col, border = NA)
-      text_char <- as.character(precip_bins$break_factor[j]) %>% gsub(pattern = ',', replacement = '-') %>% gsub(pattern = '-Inf', replacement = '+') %>% gsub(pattern = "\\(|\\]", replacement = '')
-      text(xright-bin_w/2, ybottom+bin_h/2, text_char, col = text_col, cex = 1.3)
-      xright <- xright+bin_w*shift_dir
+      x_edge <- coord_space[2] - strwidth(right_text)
+      shift_xdir <- -1
     }
 
-    # plot gage points legend
-    gage_caveat_y <- ybottom+5*bin_h*1.02
-    text(x_edge, gage_caveat_y, labels = 'Selected USGS stream gages', pos = txt_pos,
-         cex=legend_text_cfg$cex, col=legend_text_cfg$col, family=legend_text_cfg$family)
-    normal_y <- ybottom+4*bin_h
-    points(dot_x, normal_y+center_to_txt_y, pch = 21, bg = legend_styles$gage_norm_col, col = NA, cex = 2)
-    text(dot_txt_x, normal_y, labels = 'Below flood stage', pos = txt_pos,
-         cex=legend_text_cfg$cex, col=legend_text_cfg$col, family=legend_text_cfg$family)
-    flood_y <- ybottom+3*bin_h
-    points(dot_x, flood_y+center_to_txt_y, pch = 21, bg = legend_styles$gage_norm_col, col = legend_styles$gage_flood_col, lwd = 4, cex = 2)
-    text(dot_txt_x, flood_y, labels = 'Above flood stage', pos = txt_pos,
-         cex=legend_text_cfg$cex, col=legend_text_cfg$col, family=legend_text_cfg$family)
-
-    # plot storm legend
-    hurricane_y <- ybottom+6*bin_h*1.05
-    if(is.na(hurricane_col)) {
-      text(dot_txt_x, hurricane_y, labels = legend_styles$storm_name, pos = txt_pos,
-           cex=legend_text_cfg$cex, col=legend_text_cfg$col, family=legend_text_cfg$family)
-      segments(seg_x, hurricane_y+center_to_txt_y, dot_txt_x, col = legend_styles$storm_line_col, lwd = 2)
-    } else {
-      text(dot_txt_x, hurricane_y, labels = sprintf(hurricane_cat, legend_styles$storm_name), pos = txt_pos,
-           cex=legend_text_cfg$cex, col=legend_text_cfg$col, family=legend_text_cfg$family)
-      points(dot_x, hurricane_y+center_to_txt_y, pch = 21, bg = hurricane_col, col = NA, cex = 3)
+    if (y_pos == 'bottom'){
+      y_edge <- coord_space[3] + point_dia_usr*0.75 + strheight("G")*1.5 # the 1.5 multiplier captures letters below the line (e.g. g,j,p)
+      shift_ydir <- 1
+    } else if (y_pos == 'top'){
+      y_edge <- coord_space[4] - point_dia_usr*0.75 - strheight("Gg")*1.5
+      shift_ydir <- -1
     }
+
+    if(legend_cfg$y_text_pos == 'under'){
+      shift_ytext <- -1
+    } else if (legend_cfg$y_text_pos == 'over'){
+      shift_ytext <- 1
+    }
+    y_text <- y_edge + (point_dia_usr*shift_ytext) + (point_dia_usr*0.1*shift_ytext)
+
+    x_start <- x_edge + point_dia_usr/2*shift_xdir
+    for(n in 1:length(legend_cols)) {
+      x_loc <- x_start + (n-1)*point_dia_usr*shift_xdir
+      points(x_loc, y_edge, bg = paste0(legend_cols[n], alpha_hex),
+             col = legend_cols[n], pch = 21, cex = legend_cfg$point_cex, lwd = 1)
+    }
+
+    x_left <- ifelse(x_pos == "left", x_start, x_loc)
+    x_right <- ifelse(x_pos == "right", x_start, x_loc)
+
+    # Add text to show "Lower" vs "Higher"
+    text(x_left + strwidth("flows"), y_text, 'Lower flows', pos = 2,
+         col = legend_cfg$text_col, cex = legend_cfg$text_cex)
+    text(mean(c(x_start, x_loc)), y_text, 'Normal',
+         col = legend_cfg$text_col, cex = legend_cfg$text_cex)
+    text(x_right - strwidth("flows"), y_text, 'Higher flows', pos = 4,
+         col = legend_cfg$text_col, cex = legend_cfg$text_cex)
 
   }
   return(plot_fun)
