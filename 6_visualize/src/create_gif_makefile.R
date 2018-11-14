14b7672b5936a65e2bdaf609b96aaf50b5c893be
Carr <lcarr@usgs.gov>
2018-11-14 16:25:00 -0600

    move datewheel resource to the appropriate location

--- 6_visualize/src/create_gif_makefile.R
+++ 6_visualize/src/create_gif_makefile.R
@@ -5,7 +5,7 @@ create_timestep_gif_makefile <- function(makefile, task_plan, remake_file) {
     packages=c('dplyr', 'scipiper'),
     sources=c(
       '6_visualize/src/create_animation_frame.R',
-      '6_visualize/src/prep_datetime_fun.R'),
+      '6_visualize/src/prep_datewheel_fun.R'),
     file_extensions=c('feather','ind'),
     ind_complete=TRUE)
 }
