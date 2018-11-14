e2455d6a523307cef3aa27beb79db9ce9355c3a2
wdwatkins <wwatkins@usgs.gov>
2018-11-14 07:37:15 -0600

    add overall site median to dv stats

--- 2_process/src/process_dv_stats.R
+++ 2_process/src/process_dv_stats.R
@@ -10,15 +10,22 @@ process_dv_stats <- function(ind_file, dv_data_ind, site_stats_clean_ind, dates,
 
   dv_data <- readRDS(scipiper::sc_retrieve(dv_data_ind, remake_file = '1_fetch.yml'))
   site_stats <- readRDS(scipiper::sc_retrieve(site_stats_clean_ind, remake_file = '2_process.yml'))
-
   # breakdown date into month & day pairs
   dv_data_md <- dv_data %>%
     dplyr::mutate(month_nu = as.numeric(format(dateTime, "%m")),
                   day_nu = as.numeric(format(dateTime, "%d")))
+  #calculate median of medians for each site
+  assert_that(sum(is.na(site_stats$p50_va)) == 0)
+
+  #once https://github.com/USGS-VIZLAB/gage-conditions-gif/issues/48 is dealt with this
+  #assertion should be true
+  #assertthat((site_stats %>% group_by(site_no) %>% summarize(n=n()) %>% pull(n) %>% max()) == 366)
+  sites_overall_medians <- site_stats %>% group_by(site_no) %>% summarize(site_median = median(p50_va))
 
   # merge stats with the dv data
   # merge still results in extra rows - 24 extra to be exact
-  dv_with_stats <- left_join(dv_data_md, site_stats, by = c("site_no", "month_nu", "day_nu"))
+  dv_with_stats <- left_join(dv_data_md, site_stats, by = c("site_no", "month_nu", "day_nu")) %>%
+    left_join(sites_overall_medians, by = "site_no")
 
   stat_colnames <- sprintf("p%s_va", percentiles)
   stat_perc <- as.numeric(percentiles)/100
@@ -56,15 +63,13 @@ process_dv_stats <- function(ind_file, dv_data_ind, site_stats_clean_ind, dates,
     return(out)
 
   }
-
   dv_stats <- dv_with_stats %>%
     mutate(dv_val = Flow) %>%
     filter_(sprintf("!is.na(%s)", stat_colnames[1]),
             sprintf("!is.na(%s)", tail(stat_colnames,1)),
             sprintf("!is.na(%s)", "dv_val")) %>%
     mutate(per = interpolate_percentile(.)) %>%
-    select(site_no, dateTime, dv_val, per, p50_va)
-
+    select(site_no, dateTime, dv_val, per, p50_va, site_median)
   # Write the data file and the indicator file
   data_file <- scipiper::as_data_file(ind_file)
   saveRDS(dv_stats, data_file)
