1bc6af407407c6a6d153be0a70e3d3d92173cafb
Carr <lcarr@usgs.gov>
2018-12-14 13:16:00 -0600

    add check for tmp file before pulling data

--- 1_fetch/src/fetch_dv_historic.R
+++ 1_fetch/src/fetch_dv_historic.R
@@ -4,7 +4,8 @@
 #' @param sites_ind indicator file for the vector of site numbers
 #' @param dates object from viz_config.yml that specifies dates as string
 #' @param request_limit number indicating how many sites to include per dataRetrieval request (from viz_config.yml)
-fetch_dv_historic <- function(ind_file, sites_ind, dates, request_limit){
+#' @param tmp_filenames name for intermediate data files using %s syntax
+fetch_dv_historic <- function(ind_file, sites_ind, dates, request_limit, tmp_filenames){
 
   sites <- readRDS(scipiper::sc_retrieve(sites_ind, remake_file = '1_fetch.yml'))
 
@@ -13,26 +14,32 @@ fetch_dv_historic <- function(ind_file, sites_ind, dates, request_limit){
   for(i in req_bks) {
     last_site <- min(i+request_limit-1, length(sites))
     get_sites <- sites[i:last_site]
-    data_i <-
-      dataRetrieval::readNWISdata(
-        service = "dv",
-        statCd = "00003", # need this to avoid NAs
-        site = get_sites,
-        parameterCd = "00060",
-        startDate = "1800-01-01",
-        endDate = dates$start) %>%
-      dataRetrieval::renameNWISColumns()
 
-    if(nrow(data_i) > 0 && any(names(data_i) == "Flow")) {
-      data_i <- data_i[, c("site_no", "dateTime", "Flow")] # keep only dateTime and Flow columns
-    } else {
-      data_i <- NULL # no data returned situation
+    # Check if current file already exists before pulling data
+    current_filename <- sprintf(tmp_filenames, head(get_sites,1), tail(get_sites,1))
+    if(!file.exists(current_filename)) {
+      data_i <-
+        dataRetrieval::readNWISdata(
+          service = "dv",
+          statCd = "00003", # need this to avoid NAs
+          site = get_sites,
+          parameterCd = "00060",
+          startDate = "1800-01-01",
+          endDate = dates$start) %>%
+        dataRetrieval::renameNWISColumns()
+
+      if(nrow(data_i) > 0 && any(names(data_i) == "Flow")) {
+        data_i <- data_i[, c("site_no", "dateTime", "Flow")] # keep only dateTime and Flow columns
+      } else {
+        data_i <- NULL # no data returned situation
+      }
+
+      # save individual file locally as building in case something happens
+      saveRDS(data_i, current_filename)
+
+      dv_historic_data <- rbind(dv_historic_data, data_i)
     }
 
-    # save individual file locally as building in case something happens
-    saveRDS(data_i, paste0("1_fetch/tmp/historic_dv_", head(get_sites,1), "_to_", tail(get_sites,1), ".rds"))
-
-    dv_historic_data <- rbind(dv_historic_data, data_i)
     print(paste("Completed", last_site, "of", length(sites)))
   }
 
