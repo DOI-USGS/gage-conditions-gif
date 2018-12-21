2d4e30ee4c66e27c2e1ed20727f2c919b3e078ee
Carr <lcarr@usgs.gov>
2018-12-21 14:08:50 -0600

    change point style to show dry conditions

--- 2_process/src/process_dv_stat_styles.R
+++ 2_process/src/process_dv_stat_styles.R
@@ -2,52 +2,53 @@
 #'
 #' @param ind_file character file name where the output should be saved
 #' @param dv_stats_ind indicator file for the data.frame of dv_data
-#' @param color_palette list of colors to use for the color ramp (from viz_config.yml)
-#' @param size_palette range of sizes to use to scale the circles
 #' @param gage_style list indicating point type and size to use for gages with or without percentiles
-#' @param normal_percentiles vector with 2 values giving the range of percentiles to treat as "normal" condition
-process_dv_stat_styles <- function(ind_file, dv_stats_ind, color_palette, size_palette, gage_style, normal_percentiles){
+#' @param display_percentiles list with percentiles for normal_range, drought_severe, drought_low, and flood
+process_dv_stat_styles <- function(ind_file, dv_stats_ind, gage_style, display_percentiles){
 
   # read in the prepared statistics
   dv_stats <- readRDS(scipiper::sc_retrieve(dv_stats_ind, remake_file = '2_process.yml'))
+  display_percentiles_num <- lapply(display_percentiles, function(x) as.numeric(x)/100)
 
   # widen the range for "normal"
-  norm_per_low <- as.numeric(head(normal_percentiles, 1))/100
-  norm_per_high <- as.numeric(tail(normal_percentiles, 1))/100
+  norm_per_low <- min(display_percentiles_num$normal_range)
+  norm_per_high <- max(display_percentiles_num$normal_range)
   dv_stats_adj <- dv_stats %>%
     mutate(per_adj = ifelse(per >= norm_per_low & per <= norm_per_high,
                             no = per, yes = 0.5))
 
-  # define the styling functions
-  col_fun <- colorRamp(color_palette$with_percentile)
-  size_fun <- function(percentile) {
-    # to use constant dot size:
-    gage_style$with_percentile$cex
-
-    # to show seasonality through dot size:
-    #size_palette$cex_range[1] + percentile*(diff(size_palette$cex_range))
-  }
-
-  # apply the styling functions to add style columns to the data.frame
+  # set the styles
   dv_stats_with_style <- dv_stats_adj %>%
-    mutate(
-      shape = ifelse(
-        is.na(per_adj),
-        no = gage_style$with_percentile$pch,
-        yes = gage_style$no_percentile$pch),
-      size = ifelse(
-        is.na(per_adj),
-        no = size_fun(p50_quantile),
-        yes = gage_style$no_percentile$cex),
-      color = NA)
+    add_style_columns(gage_style, display_percentiles_num)
 
   # insert values into the color column. this was not working with ifelse inside
   # mutate, still NAs getting into rgb/col_fun
   per_na <- is.na(dv_stats_with_style$per_adj)
-  dv_stats_with_style$color[!per_na] <- rgb(col_fun(dv_stats_with_style$per_adj[!per_na]), maxColorValue = 255)
-  dv_stats_with_style$color[per_na] <- color_palette$no_percentile
+  dv_stats_with_style$color[per_na] <- gage_style$no_percentile$col
+  dv_stats_with_style$size[per_na] <- gage_style$no_percentile$cex
+  dv_stats_with_style$shape[per_na] <- gage_style$no_percentile$pch
 
   # Write the data file and the indicator file
   saveRDS(dv_stats_with_style, scipiper::as_data_file(ind_file))
   scipiper::gd_put(ind_file)
+}
+
+add_style_columns <- function(per_df, gage_style, percentiles) {
+  mutate(per_df,
+    color = ifelse(per <= min(percentiles$normal_range),
+                   yes = gage_style$with_percentile$drought$col,
+                   no = ifelse(per >= percentiles$flood,
+                               yes = gage_style$with_percentile$flood$col[2],
+                               no = ifelse(per >= max(percentiles$normal_range),
+                                           yes = gage_style$with_percentile$flood$col[1],
+                                           no = gage_style$with_percentile$normal$col))),
+    shape = ifelse(per <= min(percentiles$normal_range),
+                   yes = gage_style$with_percentile$drought$pch,
+                   no = gage_style$with_percentile$normal$pch),
+    size = ifelse(per <= percentiles$drought_severe,
+                  yes = max(gage_style$with_percentile$drought$cex),
+                  no = ifelse(per <= percentiles$drought_low,
+                              yes = min(gage_style$with_percentile$drought$cex),
+                              no = gage_style$with_percentile$normal$cex))
+  )
 }
