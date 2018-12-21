d6ab2da3615fb208ea2e1890ec5f83e9b88197ec
Carr <lcarr@usgs.gov>
2018-12-21 15:28:31 -0600

    adjust legend to match new styles

--- 6_visualize/src/prep_vertical_legend_fun.R
+++ 6_visualize/src/prep_vertical_legend_fun.R
@@ -1,22 +1,36 @@
 
-prep_vertical_legend_fun <- function(percentiles_str, sites_color_palette,
-                            x_pos, y_pos, legend_cfg, normal_percentiles){
+prep_vertical_legend_fun <- function(x_pos, y_pos, legend_cfg, gage_style,
+                                     display_percentiles){
 
-  col_fun <- colorRamp(sites_color_palette$with_percentile)
-  percentiles <- as.numeric(percentiles_str)/100
-  # eliminate range of adjusted "normal" colors
-  norm_per_low <- as.numeric(head(normal_percentiles, 1))/100
-  norm_per_high <- as.numeric(tail(normal_percentiles, 1))/100
-  percentiles <- percentiles[!(percentiles >= norm_per_low & percentiles < 0.50)]
-  percentiles <- percentiles[!(percentiles > 0.50 & percentiles <= norm_per_high)]
+  display_percentiles_num <- lapply(display_percentiles, function(x) as.numeric(x)/100)
+  display_percentiles_num$norm <- 0.50 # need to add so that it shows up on the legend
 
-  legend_cols <- sapply(percentiles, FUN = function(x){
-    rgb(col_fun(x), maxColorValue = 255)
-  })
+  legend_style <- data.frame(per = unlist(display_percentiles_num, use.names = FALSE)) %>%
+    add_style_columns(gage_style, display_percentiles_num) %>%
+    mutate(
+      legend_text = case_when(
+        per == display_percentiles_num$flood ~ "Wettest",
+        per == max(display_percentiles_num$normal_range) ~ "Wet",
+        per == display_percentiles_num$norm ~ "Normal",
+        per == min(display_percentiles_num$normal_range) ~ "Dry",
+        per == display_percentiles_num$drought_low ~ "Dryer",
+        per == display_percentiles_num$drought_severe ~ "Dryest",
+        TRUE ~ ""
+      )) %>%
+    arrange(desc(per)) # fill top - bottom
+
+  # Add style for missing data
+  legend_style <- legend_style %>% bind_rows(data.frame(
+    per = NA,
+    color =  gage_style$no_percentile$col,
+    shape = gage_style$no_percentile$pch,
+    size =  gage_style$no_percentile$cex,
+    legend_text = "No data"
+  ))
 
   alpha_hex <- 'CC'
 
-  rm(percentiles)
+  rm(display_percentiles_num, gage_style)
 
   plot_fun <- function(){
 
@@ -30,44 +44,26 @@ prep_vertical_legend_fun <- function(percentiles_str, sites_color_palette,
 
     # percentage of X domain for circle diameter; assumes 175 is max cex
     # used for spacing circles along the x and y directions
-    point_width <- legend_cfg$point_cex*strwidth("O")
-    point_height <- legend_cfg$point_cex*strheight("O")
-
-    # Legend text
-    bottom_text <- "Lower"
-    mid_text <- "Normal"
-    top_text <- "Higher"
-
-    y_start <- y_loc - strheight(top_text)
-    legend_cols <- rev(legend_cols) # fill top - bottom
-
-    for(n in 1:length(legend_cols)) {
-      y_loc_n <- y_start - (n-1)*point_height*0.55
-      points(x_loc, y_loc_n, bg = paste0(legend_cols[n], alpha_hex),
-             col = legend_cols[n], pch = 21, cex = legend_cfg$point_cex, lwd = 1)
-    }
-
-    # correct for shrinking text size from nested atop() calls
-    text_size <- legend_cfg$text_cex
-    text_size_bot_top <- text_size #+ 0.5
+    point_width <- legend_cfg$point_mult*strwidth("O")
+    point_height <- legend_cfg$point_mult*strheight("O")
+    legend_style$point_height <- legend_style$size*strheight("O")*legend_cfg$point_mult
 
     text_pos <- 2
-    x_text <- x_loc - point_width*0.3
+    x_text <- x_loc - point_width*0.55
 
-    y_text_midpoint <- mean(c(y_start, y_loc_n))
-    y_text_mid <- y_text_midpoint - point_height*0.06
-    y_text_bot <- y_text_midpoint - (y_text_midpoint - y_loc_n)*0.87
-    y_text_top <- y_text_midpoint + (y_start - y_text_midpoint)*0.86
-
-    # Add text to show "Lower" vs "Higher"
-    text(x_text, y_text_bot,
-         labels = bottom_text,
-         pos = text_pos, col = legend_cfg$text_col, cex = text_size_bot_top)
-    text(x_text, y_text_mid, mid_text, pos = text_pos,
-         col = legend_cfg$text_col, cex = text_size)
-    text(x_text, y_text_top,
-         labels = top_text,
-         pos = text_pos, col = legend_cfg$text_col, cex = text_size_bot_top)
+    y_start <- y_loc - strheight(legend_style$legend_text[1])
+    y_loc_n <- y_start
+    for(n in 1:nrow(legend_style)) {
+      point_height_n <- ifelse(n==1, 0, legend_style$point_height[n-1])
+      point_spacing <- ifelse(is.na(legend_style$per[n]), 0.4, 0.7)
+      y_loc_n <- y_loc_n - point_height_n*point_spacing
+      points(x_loc, y_loc_n, bg = paste0(legend_style$color[n], alpha_hex),
+             col = legend_style$color[n], pch = legend_style$shape[n],
+             cex = legend_style$size[n]*legend_cfg$point_mult, lwd = 1)
+      text(x_text, y_loc_n,
+           labels = legend_style$legend_text[n],
+           pos = text_pos, col = legend_cfg$text_col, cex = legend_cfg$text_cex)
+    }
 
   }
   return(plot_fun)
