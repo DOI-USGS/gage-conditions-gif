79d1c3ad2378bbef3dcc63f8fecc817e1f160c9e
Carr <lcarr@usgs.gov>
2019-02-08 16:40:05 -0600

    see every frame

--- 6_visualize/src/combine_animation_frames.R
+++ 6_visualize/src/combine_animation_frames.R
@@ -8,7 +8,7 @@ combine_animation_frames <- function(gif_file, animation_cfg) {
                          newName = file.path("6_visualize/tmp", paste0("frame_", countFormatted, ".png")))
   file.rename(from = file_name_df$origName, to = file_name_df$newName)
   shell_command <- sprintf(
-    "ffmpeg -y -framerate %s -i 6_visualize/tmp/frame_%%03d.png -framerate %s -pix_fmt yuv420p %s",
+    "ffmpeg -y -framerate %s -i 6_visualize/tmp/frame_%%03d.png -r %s -pix_fmt yuv420p %s",
     animation_cfg$frame_rate, animation_cfg$output_frame_rate, gif_file)
   system(shell_command)
   file.rename(from = file_name_df$newName, to = file_name_df$origName)
