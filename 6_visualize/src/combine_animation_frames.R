6b5dd22d6378cc0e02e75ef83d4a789932890251
Platt <lplatt@usgs.gov>
2019-03-15 15:54:04 -0500

    put both functions in the same file

--- 6_visualize/src/combine_animation_frames.R
+++ 6_visualize/src/combine_animation_frames.R
@@ -1,3 +1,19 @@
+combine_animation_frames_video <- function(out_file, animation_cfg) {
+  #build video from pngs with ffmpeg
+  #note that this will use all frames in 6_visualize/tmp
+  #have to rename files since can't use globbing with ffmpeg on Windows :(
+  png_frames <- list.files('6_visualize/tmp', full.names = TRUE)
+  file_name_df <- tibble(origName = png_frames,
+                         countFormatted = zeroPad(1:length(png_frames), padTo = 3),
+                         newName = file.path("6_visualize/tmp", paste0("frame_", countFormatted, ".png")))
+  file.rename(from = file_name_df$origName, to = file_name_df$newName)
+  shell_command <- sprintf(
+    "ffmpeg -y -framerate %s -i 6_visualize/tmp/frame_%%03d.png -r %s -pix_fmt yuv420p %s",
+    animation_cfg$frame_rate, animation_cfg$output_frame_rate, out_file)
+  system(shell_command)
+  file.rename(from = file_name_df$newName, to = file_name_df$origName)
+}
+
 combine_animation_frames_gif <- function(out_file, animation_cfg) {
   #build gif from pngs with magick and simplify with gifsicle
   #note that this will use all frames in 6_visualize/tmp
