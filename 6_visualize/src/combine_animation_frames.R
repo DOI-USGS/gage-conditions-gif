7361c7899d17fe161d4b1abcc5ae4877166dd7b6
Platt <lplatt@usgs.gov>
2020-04-17 13:22:16 -0500

    use an intermediate video before downsizing

--- 6_visualize/src/combine_animation_frames.R
+++ 6_visualize/src/combine_animation_frames.R
@@ -14,9 +14,9 @@ combine_animation_frames_video <- function(out_file, animation_cfg) {
 
   shell_command <- sprintf(
     "ffmpeg -y -framerate %s -i 6_visualize/tmp/frame_%%03d.png -r %s -pix_fmt yuv420p -vcodec libx264 -crf 27 %s",
-    animation_cfg$frame_rate, animation_cfg$output_frame_rate, out_file)
+    animation_cfg$frame_rate, animation_cfg$output_frame_rate, "6_visualize/tmp/pre_downsized.mp4")
   system(shell_command)
-  system(sprintf("ffmpeg -i %s -vf scale=iw/2:-1 %s", out_file, out_file))
+  system(sprintf("ffmpeg -i %s -vf scale=iw/2:-1 %s", "6_visualize/tmp/pre_downsized.mp4", out_file))
 
   file.rename(from = file_name_df$newName, to = file_name_df$origName)
 }
